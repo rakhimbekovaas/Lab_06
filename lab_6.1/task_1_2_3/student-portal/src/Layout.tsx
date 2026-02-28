4bd76b8966f49a9f0a7e8e613cde00812a259cd9
rakhimbekovaas <supersana121207@gmail.com>
2026-02-28 09:53:49 +0500

    fix: Link issue

--- lab_6.1/task_1_2_3/student-portal/src/Layout.tsx
+++ lab_6.1/task_1_2_3/student-portal/src/Layout.tsx
@@ -1,12 +1,12 @@
-import { Link, NavLink, Outlet } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom"
 // Task_3
 function Layout() {
     return (
         <>
             <nav style={{ display: "flex", gap: "1rem", padding: "1rem", borderBottom: "1px solid #ccc"}}>
                 <Link to = "/">Home</Link>
-                <NavLink to = "/courses" style={({ isActive}) => ({fontWeight: isActive ? "bold": "normal"})}>Courses</NavLink>
-                <NavLink to = "/about" style={({ isActive}) => ({fontWeight: isActive ? "bold": "normal"})}>About</NavLink>
+                <Link to = "/courses">Courses</Link>
+                <Link to = "/about">About</Link>
             </nav>
             <main style={{ padding: "2rem"}}>
                 <Outlet />
